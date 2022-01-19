832daa04c1897569cfdb1b78b96305ded0579277
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-19 07:05:34 +0200

    Creating tables

--- lib/data/sqlite/database_helper.dart
+++ lib/data/sqlite/database_helper.dart
@@ -5,6 +5,7 @@ import 'package:sqlbrite/sqlbrite.dart';
 import 'package:synchronized/synchronized.dart';
 import '../models/models.dart';
 
+// This class will handle all the SQLite database operations.
 class DatabaseHelper {
   // Constants for the database name and version.
   static const _databaseName = 'MyRecipes.db';
@@ -20,6 +21,7 @@ class DatabaseHelper {
   static late BriteDatabase _streamDatabase;
 
   // make this a singleton class
+
   //Make the constructor private and provide a public static instance.
   DatabaseHelper._privateConstructor();
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
@@ -30,6 +32,32 @@ class DatabaseHelper {
   // Private sqflite database instance.
   static Database? _database;
 
-  // TODO: Add create database code here
+  // SQL code to create the database table
+  // Pass an sqflite database db into the method. It will create the tables.
+  Future _onCreate(Database db, int version) async {
+    // Create recipeTable with the same columns as the model using CREATE TABLE.
+    await db.execute('''
+        CREATE TABLE $recipeTable (
+          $recipeId INTEGER PRIMARY KEY,
+          label TEXT,
+          image TEXT,
+          url TEXT,
+          calories REAL,
+          totalWeight REAL,
+          totalTime REAL
+        )
+        ''');
+    // Create ingredientTable.
+    await db.execute('''
+        CREATE TABLE $ingredientTable (
+          $ingredientId INTEGER PRIMARY KEY,
+          $recipeId INTEGER,
+          name TEXT,
+          weight REAL
+        )
+        ''');
+  }
+
+// TODO: Add code to open database
 
 }
